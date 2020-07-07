3b7f84ef487f2857651284f8c91ad65079b82501
Victor Alonso Altamirano Izquierdo <v.a.i@hotmail.com>
2020-07-07 21:50:21 +0100

    first

---
+++ precache-manifest.e5c59af2931c88f19cda516cb96d0b65.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d4e10545d8b691f677469392681ef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fefa6d9cfda744f165e",
+    "url": "/static/css/2.b8d2e23a.chunk.css"
+  },
+  {
+    "revision": "3acb14c06322d8fd5bef",
+    "url": "/static/css/main.97b784d1.chunk.css"
+  },
+  {
+    "revision": "3fefa6d9cfda744f165e",
+    "url": "/static/js/2.2c7ebbfa.chunk.js"
+  },
+  {
+    "revision": "ac6dd135d9bfc71ef216d9e20e47b4cb",
+    "url": "/static/js/2.2c7ebbfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3acb14c06322d8fd5bef",
+    "url": "/static/js/main.3f7a23f7.chunk.js"
+  },
+  {
+    "revision": "e531356e853faf64aa8d",
+    "url": "/static/js/runtime-main.922033bd.js"
+  },
+  {
+    "revision": "5a0a00aeebb46187bb2c9cb79d7fc4ba",
+    "url": "/static/media/heart.5a0a00ae.gif"
+  },
+  {
+    "revision": "b6054df234a3eee04d2d9bed47394442",
+    "url": "/static/media/hum.b6054df2.gif"
+  },
+  {
+    "revision": "e5b45a1e07ce7f13481a94266985d514",
+    "url": "/static/media/iot.e5b45a1e.jpg"
+  },
+  {
+    "revision": "78d655ea2bdd44354a2f46fe80623784",
+    "url": "/static/media/list.78d655ea.jpg"
+  },
+  {
+    "revision": "ae8285c348f83327e8ea107252fa4e35",
+    "url": "/static/media/logo.ae8285c3.svg"
+  },
+  {
+    "revision": "64448821eddd45fc8190fb557e9be71c",
+    "url": "/static/media/lux.64448821.gif"
+  },
+  {
+    "revision": "dc43d256088657a0f3616fa04398e5e1",
+    "url": "/static/media/pointer.dc43d256.gif"
+  },
+  {
+    "revision": "d47f6b56cd3cb4998bac586635f5e9e9",
+    "url": "/static/media/run.d47f6b56.gif"
+  },
+  {
+    "revision": "e0b34c5bc52b654409bfd1ab67c4bb5c",
+    "url": "/static/media/sleep.e0b34c5b.gif"
+  },
+  {
+    "revision": "caf598cec1cc68159cf46f5763ea9370",
+    "url": "/static/media/temp.caf598ce.gif"
+  }
+]);
